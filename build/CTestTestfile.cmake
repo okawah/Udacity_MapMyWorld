de5d2d5b57b56a0b7a13c5aebd72599dff735ad6
root <okawah@student.ethz.ch>
2019-08-04 21:42:14 +0000

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/catkin_ws/src
+# Build directory: /home/workspace/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(Udacity_MapMyWorld/src/where_am_i/src/my_robot)
+subdirs(teleop_twist_keyboard)
+subdirs(Udacity_MapMyWorld/src/where_am_i/src/ball_chaser)
